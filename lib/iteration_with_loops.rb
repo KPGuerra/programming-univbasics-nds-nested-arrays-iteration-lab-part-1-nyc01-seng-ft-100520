22e5d263a0100588fabe8b1304937102049aa763
Karina Guerra <kpguerra96@gmail.com>
2020-09-21 00:44:26 +0000

    Automatically backed up by Learn

--- lib/iteration_with_loops.rb
+++ lib/iteration_with_loops.rb
@@ -6,7 +6,7 @@ def find_even_values(array_of_arrays)
   while count < array_of_arrays.length do
     inner_count = 0
     while inner_count < array_of_arrays[count].length do
-      if array_of_arrays[count][inner_count] % 2 = 0
+      if array_of_arrays[count][inner_count] % 2 == 0
         p array_of_arrays[count][inner_count]
       end 
       inner_count += 1
